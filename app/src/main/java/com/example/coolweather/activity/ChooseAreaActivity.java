6b0123bbc17debb8c5c730d4d2225905004d021b
lswyfan <a86878@gmail.com>
2017-02-05 17:26:29 +0800

    ca activity

---
+++ app/src/main/java/com/example/coolweather/activity/ChooseAreaActivity.java
@@ -0,0 +1,10 @@
+package com.example.coolweather.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by Administrator on 2017/2/5.
+ */
+
+public class ChooseAreaActivity extends Activity {
+}
